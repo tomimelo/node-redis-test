1ff7dcdc69a4de821a086e1eafd1b98d4c19b424
Tomas Melone <tomasmelone@hotmail.com>
2022-01-28 13:48:27 +0100

    feat: Uncomment server start

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
+import Server from "./api/server"
+
 (async () => {
   try {
     const port = process.env.PORT || 3000
 
     console.log("Starting server...")
 
-    // const server = new Server(port)
-    // server.start()
+    const server = new Server(port)
+    server.start()
   } catch (error) {
     console.log(error)
     process.exit(1)
