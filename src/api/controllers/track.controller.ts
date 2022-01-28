4c190edc5567615c6a07f9b9baf2ca2db1362932
Tomas Melone <tomasmelone@hotmail.com>
2022-01-28 22:39:27 +0100

    feat: Save data

--- src/api/controllers/track.controller.ts
+++ src/api/controllers/track.controller.ts
@@ -1,12 +1,17 @@
 import { NextFunction, Request, Response } from 'express'
 import { createCustomError } from '../../utils/helpers'
+import FileService from '../services/file.service'
 
 export const trackController = {
-  trackData: (req: Request, res: Response, next: NextFunction) => {
+  trackData: async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const data = req.body
+
+      await FileService.saveData(data)
+
       res.json({
         ok: true,
-        msg: 'Ok!'
+        msg: 'Data tracked successfully'
       })
     } catch (error) {
       next(createCustomError(error, 1000))
