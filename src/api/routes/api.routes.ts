d26ebafcc2f2ba50186220bdd2e3680426e3d1f4
Tomas Melone <tomasmelone@hotmail.com>
2022-01-28 14:07:24 +0100

    feat: Add routes

--- src/api/routes/api.routes.ts
+++ src/api/routes/api.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 
+import countRoutes from './count.routes'
+import trackRoutes from './track.routes'
+
 const router = Router()
 
-// router.use('/count', countRoutes)
-// router.use('/track', trackRoutes)
+router.use('/count', countRoutes)
+router.use('/track', trackRoutes)
 
 export default router
