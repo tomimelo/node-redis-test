027fe774c30c9971f248077dc9848f430aea8824
Tomas Melone <tomasmelone@hotmail.com>
2022-01-28 13:40:40 +0100

    feat: Create app initializer

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+(async () => {
+  try {
+    const port = process.env.PORT || 3000
+
+    console.log("Starting server...")
+
+    // const server = new Server(port)
+    // server.start()
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+})()
