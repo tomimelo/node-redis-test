7cccc0f09f1fbd106ecca70abb873ac23e502317
Tomas Melone <tomasmelone@hotmail.com>
2022-01-30 00:58:03 +0100

    feat: Save count in track controller

--- src/api/controllers/track.controller.ts
+++ src/api/controllers/track.controller.ts
@@ -1,13 +1,16 @@
 import { NextFunction, Request, Response } from 'express'
 import { createCustomError } from '../../utils/helpers'
 import FileService from '../services/file.service'
+import RedisClient from '../services/redis.service'
 
 export const TrackController = {
   trackData: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const data = req.body
-
       await FileService.saveData(data)
+      if (Object.prototype.hasOwnProperty.call(data, 'count')) {
+        await RedisClient.saveCount(data.count)
+      }
 
       res.json({
         ok: true,
