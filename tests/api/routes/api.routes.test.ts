e7519a502bb51ecc767809a93f0253d8fe0b2de6
Tomas Melone <tomasmelone@hotmail.com>
2022-01-30 22:42:42 +0100

    test: Create more tests

--- tests/api/routes/api.routes.test.ts
+++ tests/api/routes/api.routes.test.ts
@@ -13,12 +13,30 @@ describe('API routes', () => {
     await redisClient.disconnect()
   })
 
-  const randomNumbers = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100) + 1)
-  randomNumbers.forEach(async (count) => {
-    test(`Should get count = ${count}`, async () => {
-      await redisClient.set('count', count)
+  const methods = ['get', 'post', 'put', 'delete']
+  methods.forEach(method => {
+    test(`Should not found undefined ${method.toUpperCase()} route`, async () => {
+      const route = '/api/not-found'
+      const res = await (api as any)[method](route).expect(404)
+      expect(res.body?.error?.status).toBe(404)
+      expect(res.body?.error?.message).toBe(`${method.toUpperCase()} ${route} not found`)
+    })
+  })
+
+  describe('Count route', () => {
+    const randomNumbers = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100) + 1)
+    randomNumbers.forEach(count => {
+      test(`Should get count = ${count}`, async () => {
+        await redisClient.set('count', count)
+        const response = await api.get('/api/count').expect(200)
+        expect(response.body.count).toBe(count)
+      })
+    })
+
+    test('Should get count = 0 if key count does not exist', async () => {
+      await redisClient.del('count')
       const response = await api.get('/api/count').expect(200)
-      expect(response.body.count).toBe(count)
+      expect(response.body.count).toBe(0)
     })
   })
 })
